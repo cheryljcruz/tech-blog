262298c82e4bf986b80fdf902785232b7284fe34
koiclone <cheryljcruz1@gmail.com>
2022-05-12 23:15:55 -0500

    handlebars and javascript logic

---
+++ public/javascript/add-post.js
@@ -0,0 +1,27 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body_text = document.querySelector('input[name="post-body_text"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        post_url,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document
+    .querySelector(".new-post-form")
+    .addEventListener("submit", newFormHandler);
